db80290c84349513d557fe17d852dc4cee670340
bluefoxbandido <adamthewonderboy@gmail.com>
2019-12-20 17:35:53 -0600

    recieving data

--- src/components/movie-manager.js
+++ src/components/movie-manager.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react'
+import StarRating from '../movie-manager/star.rating'
 
-const MovieManager = () => {
-    return (
-        <div className='movieManager'>
-            Movie Manager Goes here
-        </div>
-    )
+export default class MovieManager extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            movies: []
+        }
+    }
+
+    componentDidMount() {
+        fetch(`https://infinite-hamlet-24489.herokuapp.com/get/movies`)
+            .then(res => res.json())
+            .then(movies => {
+                this.setState({
+                    movies: movies
+                })
+            })
+            .catch(error => {
+                console.log('Error', error)
+            })
+
+    }
+
+
+    render() {
+        const movieList = this.state.movies.map(movie => {
+            console.log(movie)
+            return (
+                    <div key={movie[0]} className="movie">
+                        <div className="title">
+                            <h2>{movie[1]}</h2>
+                            <p>
+                                {movie[3]}
+                            </p>
+                        </div>
+                    </div>
+            )
+        })
+        return (
+            <div className=''>
+                {movieList}
+            </div>
+        )
+    }
 }
-
-export default MovieManager;
